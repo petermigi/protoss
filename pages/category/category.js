c37c4aa07eb6f601e21b673e75ed78f709fb8272
peterliu <3102415881@qq.com>
2019-01-04 09:35:26 +0800

    11-22商品分类页面二  课程源代码中把category分类页面数据绑定时把products写成了procucts, 我自己做数据绑定用products与源代码不同

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,4 +1,7 @@
-// pages/category/category.js
+import { Category } from 'category-model.js';
+var category = new Category();
+
+
 Page({
 
     /**
@@ -12,7 +15,31 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        this._loadData();
+    },
 
+    _loadData: function(){
+        category.getCategoryType((categoryData)=>{
+            this.setData({
+                categoryTypeArr:categoryData
+            });
+
+        // 一定要在回调函数里再进行获取分类详情的方法调用
+        category.getProductsByCategory(categoryData[0].id, (data)=>{
+
+            var dataObj = {
+                products: data, 
+                topImgUrl: categoryData[0].img.url,
+                title: categoryData[0].name
+            };
+
+            this.setData({
+                categoryProducts:dataObj
+            });
+        });
+        });
+
+        
     },
 
     /**
@@ -20,47 +47,7 @@ Page({
      */
     onReady: function () {
 
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
     }
+
+    
 })
