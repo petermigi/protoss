f272481d2348b0aab67dbfd50c4f8f55e6800018
peterliu <3102415881@qq.com>
2019-02-26 17:13:51 +0800

    13-13支付后刷新订单详情页面

--- pages/order/order.js
+++ pages/order/order.js
@@ -14,7 +14,7 @@ Page({
      * 页面的初始数据
      */
     data: {
-        id: -1
+        id: null
     },
 
     /**
@@ -40,6 +40,36 @@ Page({
             this._bindAddressInfo(res);
         });
     },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+        if(this.data.id) {
+            var that = this;
+            //下单后，支付成功或者失败后，点左上角返回时能够更新订单状态 所以放在onshow中
+            var id = this.data.id;
+            order.getOrderInfoById(id, (data)=> {
+                that.setData({
+                    orderStatus: data.status,
+                    productsArr: data.snap_items,
+                    account: data.total_price,
+                    basicInfo: {
+                        orderTime: data.create_time,
+                        orderNo: data.order_no
+                    },
+                });
+
+                // 快照地址
+                var addressInfo=data.snap_address;
+                addressInfo.totalDetail = address.setAddressInfo(addressInfo);
+                that._bindAddressInfo(addressInfo);
+            });
+        }
+
+    },
+
 
     editAddress: function(){
         var that = this;
@@ -220,54 +250,7 @@ Page({
         this.setData({
             addressInfo: addressInfo
         });
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
     }
+
+    
 })
